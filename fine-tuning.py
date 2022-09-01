6b12ee6d6da03612e8a6baad5c735ec0b18598d3
xdwang0726 <starwxd@gmail.com>
2022-09-01 10:53:53 +0800

    update path

--- fine-tuning.py
+++ fine-tuning.py
@@ -143,7 +143,7 @@ def main():
     parser.add_argument("--out_dir", type=str, default="checkpoints")
     parser.add_argument("--result_dir", type=str, default="supervised_learning_results")
 
-    parser.add_argument('--imbalance_level', type=str, default='low',
+    parser.add_argument('--level', type=str, default='low',
                         help="imbalance level of labels, choosing from low, medium, high")
     parser.add_argument('--label_imbalance', type=bool, default=False)
 
@@ -163,7 +163,11 @@ def main():
     seeds = args.seeds.split(",")
 
     # get tuned hyperparameter
-    para_path = os.path.join(args.para_dir, "{}.json".format(args.dataset))
+    if not args.label_imbalance:
+        para_path = os.path.join(args.para_dir, "noisy_label", "{}.json".format(args.dataset))
+    else:
+        para_path = os.path.join(args.para_dir, "label_imbalance", "{}.json".format(args.dataset))
+
     with open(para_path, "r") as f:
         para = json.load(f)
 
