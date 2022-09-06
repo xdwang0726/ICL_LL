15ac65382fa1c1aa0b94a411db50754d02e3319f
xdwang0726 <starwxd@gmail.com>
2022-09-06 16:52:32 +0800

    test add options to prompt examples

--- icl/data_new_prompt.py
+++ icl/data_new_prompt.py
@@ -393,7 +393,7 @@ class MetaICLData(object):
                     dp, is_first=i==0, for_demonstrations=True,
                     add_newlines=add_newlines)
                 demonstrations += input_ + output_
-                print('demonstration example:', demonstrations)
+                print('demonstration example:', self.tokenizer(demonstrations))
 
         for dp_idx, dp in enumerate(test_data):
             inputs, outputs, answer = self._prepro_each_datapoint(
