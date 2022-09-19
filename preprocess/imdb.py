937090e8664a059bfb09c37d5131d8d106dd6413
xdwang0726 <starwxd@gmail.com>
2022-09-19 14:46:13 +0800

    add imdb

--- preprocess/imdb.py
+++ preprocess/imdb.py
@@ -46,7 +46,7 @@ def main():
     dataset = IMDB()
 
     for seed in [100, 13, 21, 42, 87]:
-        train, dev, test = dataset.generate_k_shot_data(k=16, seed=seed, path="../data_imbalance/")
+        train, dev, test = dataset.generate_k_shot_data(k=16, seed=seed, path="../data_noisy_label/")
 
 if __name__ == "__main__":
     main()
