e4a1f173aeb51efce485102b887aefe13e5038fe
xdwang0726 <starwxd@gmail.com>
2022-09-21 17:44:09 +0800

    model parallel

--- grid_search.py
+++ grid_search.py
@@ -157,12 +157,8 @@ def hyperparameter_tuning(args, device, train_path, test_path, para_dict, collat
         model.config.pad_token_id = model.config.eos_token_id
     elif args.gpt2.startswith("gpt-j"):
         model_config = GPTJConfig.from_pretrained("EleutherAI/gpt-j-6B", num_labels=num_label)
-        model = GPTJForSequenceClassification.from_pretrained("EleutherAI/gpt-j-6B",  low_cpu_mem_usage=True, config=model_config)
-        device_map = {0: [0, 1, 2, 3, 4, 5, 6],
-                      1: [7, 8, 9, 10, 11, 12, 13],
-                      2: [14, 15, 16, 17, 18, 19, 20],
-                      3: [21, 22, 23, 24, 25, 26, 27]}
-        model.parallelize(device_map)
+        model = GPTJForSequenceClassification.from_pretrained("EleutherAI/gpt-j-6B",  device_map="auto", low_cpu_mem_usage=True, config=model_config)
+
 
     model.config.pad_token_id = model.config.eos_token_id
     model.to(device)
