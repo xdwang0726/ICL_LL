46679c3eb7e3ac83fe5be1c480174bd03eff8740
xdwang0726 <starwxd@gmail.com>
2022-09-22 15:49:28 +0800

    model parallel

--- grid_search_distributed.py
+++ grid_search_distributed.py
@@ -188,7 +188,7 @@ def hyperparameter_tuning(args, device, train_path, test_path, para_dict, collat
         0: [0, 1, 2, 3, 4, 5, 6],
         1: [7, 8, 9, 10, 11, 12, 13],
         2: [14, 15, 16, 17, 18, 19, 20],
-        3: [21, 22, 23, 24, 25, 26, 27],
+        3: [21, 22, 23, 24, 25, 26, 27, 28],
     }
     model.config.pad_token_id = model.config.eos_token_id
     model.to(device)
