31ef86696f340701bcc74b0978ad7c78183a98c7
xdwang0726 <starwxd@gmail.com>
2022-10-19 14:39:04 +0800

    calculate the imbalance ratio for noisy labels

--- calculate_distribution.py
+++ calculate_distribution.py
@@ -25,7 +25,6 @@ def main():
                 dp = json.loads(line)
                 if dp["output"] in train_labels:
                     train_labels[dp["output"]] += 1
-
         num_labels = list(train_labels.values()).sort()
         print(num_labels)
         largest = num_labels[-1]
